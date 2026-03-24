898f7da6be18e7aac148f1db2729323ed3d36be7
DeBaarse <spyfonos@gmail.com>
2026-03-24 12:02:41 +0100

    Refactor genome processing functions in backend.py

--- backend.py
+++ backend.py
@@ -1,7 +1,7 @@
 import subprocess, sys
 
 file_location = sys.argv[1]
-class Run_program():
+
 
 
 
@@ -15,18 +15,28 @@ def minimap2():
         sys.exit(f"poging tot analyse gefaald {e.returncode} tijdens het indexen van je genoom via minimap2")
 
 
-def samtools_sort():
+def samtools_sort(minimap2_output):
     try:
         output = subprocess.run(
-            ["minimap2", "-d", "ref.mmi", file_location], check=True, text=True)
+            ["samtools", "sort", "-o", 'output_samtools_sorted', minimap2_output], check=True, text=True)[3]
         return output
 
     except subprocess.CalledProcessError as e:
-        sys.exit(f"poging tot analyse gefaald {e.returncode} tijdens het indexen van je genoom via samtools")
+        sys.exit(f"poging tot analyse gefaald {e.returncode} tijdens het sorten van je genoom via samtools")
+
+def samtools_index(samtools_sort_output):
+    try:
+        output = subprocess.run(
+            ["samtools", "index", samtools_sort_output], check=True, text=True)
+        return output
+
+    except subprocess.CalledProcessError as e:
+        sys.exit(f"poging to analyse gefaald {e.returncode} tijdens het indexen van je genoom via samtools")
+
+def
 
 
-#minimap2 -d ref.mmi reference.fasta
-#samtools index your_file.bam
 def main():
     minimap2_output = minimap2()
-    samtools_sort(minimap2_output)
+    samtools_sort_output = samtools_sort(minimap2_output)
+    samtools_index(samtools_sort_output)
